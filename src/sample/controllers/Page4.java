0e43d007a06021585af6dc4903945541d19b1513
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2021-01-14 13:41:19 +0100

    removed unused code made CSS style more specific
    minor improvements

--- src/sample/controllers/Page4.java
+++ src/sample/controllers/Page4.java
@@ -1,10 +1,8 @@
 package sample.controllers;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableView;
-import sample.beans.SudokuRowBean;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,26 +10,12 @@ import java.util.ResourceBundle;
 public class Page4 implements Initializable {
 
     @FXML
-    private TableView<SudokuRowBean> table;
+    TextArea textArea;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        ObservableList<SudokuRowBean> sudokuRowBeans = table.getItems();
-
-        for (int i = 0; i < 9; i++) {
-            SudokuRowBean rowBean = new SudokuRowBean();
-            rowBean.setCol0("0");
-            rowBean.setCol1("1");
-            rowBean.setCol2("2");
-            rowBean.setCol3("3");
-            rowBean.setCol4("4");
-            rowBean.setCol5("5");
-            rowBean.setCol6("6");
-            rowBean.setCol7("7");
-            rowBean.setCol8("8");
-            sudokuRowBeans.add(rowBean);
-        }
+        textArea.setText("AAAA");
 
     }
 
