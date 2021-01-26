89ba34070c10af55ac3d37b6a1826c49530dd88e
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2021-01-26 13:44:07 +0100

    Sudoku page: added cells as buttons

--- src/sample/controllers/Sudoku.java
+++ src/sample/controllers/Sudoku.java
@@ -1,25 +1,17 @@
 package sample.controllers;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextArea;
-import sample.SudokuValueGenerator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Sudoku implements Initializable {
 
-    @FXML
-    TextArea textArea;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        Integer[][] values = new SudokuValueGenerator().generate();
-        String valuesAsString = renderIntegerMatrixAsString(values);
-
-        textArea.setText(valuesAsString);
+        // Integer[][] values = new SudokuValueGenerator().generate();
+        // String valuesAsString = renderIntegerMatrixAsString(values);
 
     }
 
